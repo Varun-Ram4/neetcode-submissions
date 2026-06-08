3ee0e4392b02abb0c8eaa7f0ea74bfa4fc2448c4
Varun-Ram4 <ramabathren.ln@gmail.com>
2026-06-09 00:18:00 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length()-1;
+        while (left < right){
+            while (left < right && !isAlphaNum(s.charAt(left))){
+                left++;
+            }
+            while (right > left && !isAlphaNum(s.charAt(right))){
+                right--;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+            
+        }
+         
+        return true;
+    }
+    public boolean isAlphaNum(char c){
+        return (('A'<= c && c <= 'Z')||
+        ('a'<= c && c <= 'z')||
+        ('0'<=c && c <='9'));
+    }
+}
